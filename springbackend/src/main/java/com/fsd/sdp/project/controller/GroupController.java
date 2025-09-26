a1ab6c854b5ce2528b0dfe8bff26d7f7deae959c
varshith84 <112600740+varshith84@users.noreply.github.com>
2025-09-26 19:49:41 +0530

    Update GroupController.java

--- springbackend/src/main/java/com/fsd/sdp/project/controller/GroupController.java
+++ springbackend/src/main/java/com/fsd/sdp/project/controller/GroupController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000/", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:9090/", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/groups")
 public class GroupController {
