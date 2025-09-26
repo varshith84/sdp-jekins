eec51ca4ff363c52c67f6d3681233abdc2508e13
varshith84 <112600740+varshith84@users.noreply.github.com>
2025-09-26 19:49:04 +0530

    Update ProjectApplication.java

--- springbackend/src/main/java/com/fsd/sdp/project/ProjectApplication.java
+++ springbackend/src/main/java/com/fsd/sdp/project/ProjectApplication.java
@@ -3,14 +3,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
@@ -23,7 +23,7 @@ public WebMvcConfigurer corsConfigurer() {
         public void addCorsMappings(CorsRegistry registry) {
             // Allow all paths starting with /api
             registry.addMapping("/api/**")
-                    .allowedOrigins("http://localhost:3000")  // Allow any origin for debugging
+                    .allowedOrigins("http://localhost:9090/")  // Allow any origin for debugging
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowCredentials(true);
         }
